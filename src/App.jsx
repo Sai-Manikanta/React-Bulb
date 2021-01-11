c2a5be503dce4369c690432d6aa35ed619bbbdd5
mani333007 <mani333007@gmail.com>
2021-01-11 21:09:29 +0530

    Added heading, icon and basic styles
    icon from font awesome and styles at index.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
+
 const App = () => {
     return ( 
         <div className="app">
-            App
+            <h1>Bulb</h1>
+            <div className="bulb-container">
+                <FontAwesomeIcon icon={faLightbulb} style={{ color: 'yellow', fontSize: '20rem' }} />
+            </div>
         </div>
      );
 }
