fe04d7970fbefb596bc92bd227e2445c4f93cf93
mani333007 <mani333007@gmail.com>
2021-01-11 21:34:40 +0530

    Added on off functionality of bulb

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
+import { faLightbulb, faToggleOn, faToggleOff } from '@fortawesome/free-solid-svg-icons'
 
 const App = () => {
+    const [bulbStatus, setBulbStatus] = useState(false);
+
     return ( 
         <div className="app">
             <h1>Bulb</h1>
             <div className="bulb-container">
-                <FontAwesomeIcon icon={faLightbulb} style={{ color: 'yellow', fontSize: '20rem' }} />
+                <FontAwesomeIcon icon={faLightbulb} style={{ color: bulbStatus ? 'yellow' : '#f1f1f1', fontSize: '20rem' }} />
+                <button onClick={() => setBulbStatus(!bulbStatus)}>
+                    <FontAwesomeIcon icon={bulbStatus ? faToggleOn : faToggleOff} />
+                    {bulbStatus ? ' on' : ' off' }
+                </button>
             </div>
         </div>
      );
